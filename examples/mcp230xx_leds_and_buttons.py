480429d81d5781500db169bee419b8530bc6f414
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-06-27 17:07:38 +0100

    Apply SPDX tagging with license and copyright information.
    This makes the project comply with the REUSE.software specifications.

--- examples/mcp230xx_leds_and_buttons.py
+++ examples/mcp230xx_leds_and_buttons.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
 import board
 import busio
 from digitalio import Direction, Pull
--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
 """A setuptools based setup module.
 
 See:
