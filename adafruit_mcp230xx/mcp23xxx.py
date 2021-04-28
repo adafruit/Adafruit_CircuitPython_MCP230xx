f7dbc4768763a1c5776f903930f0510ec8ba75f5
Red_M <git@red-m.net>
2021-04-28 18:42:48 +1000

    Remove unnecessary variables for mcp23xxx.py

--- adafruit_mcp230xx/mcp23xxx.py
+++ adafruit_mcp230xx/mcp23xxx.py
@@ -17,14 +17,6 @@ from adafruit_bus_device import i2c_device, spi_device
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MCP230xx.git"
 
-# Global buffer for reading and writing registers with the devices.  This is
-# shared between both the MCP23008 and MCP23017 class to reduce memory allocations.
-# However this is explicitly not thread safe or re-entrant by design!
-_BUFFER = bytearray(3)
-MCP23SXX_CODE_READ = 0x41
-MCP23SXX_CODE_WRITE = 0x40
-
-
 # pylint: disable=too-few-public-methods
 class MCP23XXX:
     """Base class for MCP230xx devices."""
